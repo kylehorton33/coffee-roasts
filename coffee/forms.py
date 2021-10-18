ddbb0b822dc73247298c2e2fb1c54a628049052a
Kyle Horton <kyle@cplventures.com>
2021-10-18 18:22:41 -0400

    remove help texts after creation

--- coffee/forms.py
+++ coffee/forms.py
@@ -6,9 +6,9 @@ class NewRoastModelForm(ModelForm):
         model = Roast
         fields = ['roaster', 'bean', 'green_quantity', 'roasted_on', 'time_to_first_crack', 'time_to_cooling', 'roast_quantity', 'roast_level']
         help_texts = {
-            'roasted_on': ('Click \'START\' to begin roast'),
-            'time_to_first_crack': ('Click \'1st CRACK\''),
-            'time_to_cooling': ('Click \'COOL\''),
+            'roasted_on': ('<span id="help_start">Click \'START\' to begin roast<span>'),
+            'time_to_first_crack': ('<span id="help_crack">Click \'1st CRACK\'<span>'),
+            'time_to_cooling': ('<span id="help_cool">Click \'COOL\'<span>'),
         }
         widgets = {
          'roast_level': TextInput(attrs={
